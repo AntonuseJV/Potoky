7f148ce94ab2c80cec3fe795828193c0b34475cf
AntonuseJV <113193545+AntonuseJV@users.noreply.github.com>
2023-01-31 16:31:40 +0300

    Delete src/main/java directory

--- src/main/java/utils/exceptions/SettingsFileReadException.java
+++
@@ -1,8 +0,0 @@
-package utils.exceptions;
-
-public class SettingsFileReadException extends RuntimeException {
-    public SettingsFileReadException(Exception e) {
-        super("Settings file (shop.xml) can't read!", e);
-    }
-}
-
